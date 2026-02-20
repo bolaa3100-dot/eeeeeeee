96f60da69ab7cacfc23803d174bdb4fcdf8f17d9
Bibo <bolaatef20061@gmail.com>
2026-02-20 23:42:49 +0200

    Add

--- lib/core/colors.dart
+++ lib/core/colors.dart
@@ -3,4 +3,5 @@ import 'package:flutter/material.dart';
 class Colors {
   static const bl = Color(0xffffffff);
   static const bla = Color(0xffffffff);
+  static const blasss = Color(0xffffffff);
 }
